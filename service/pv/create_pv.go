89fb280d3a304195c83ccc876ea7298a7016db6e
root <root@localhost.localdomain>
2023-01-03 17:18:53 +0800

    pv创建优化

--- service/pv/create_pv.go
+++ service/pv/create_pv.go
@@ -3,39 +3,39 @@ package service
 import (
 	"context"
 	"errors"
-	"k8s.io/apimachinery/pkg/api/resource"
 	"strings"
 
 	"go.uber.org/zap"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 )
 
 type PvCreate struct {
-	Name         string            `json:"name"`
-	Namespace    string            `json:"namespace"`
-	Cluster      string            `json:"cluster"`
-	Data         map[string]string `json:"data"`
-	StorageClass string            `json:"storage_class"`
-	VolumeMode   string            `json:"volume_mode"`
-	Storage      string            `json:"storage"`
-	AccessMode   string            `json:"access_mode"`
-	Path         string            `json:"path"`
+	Name         string `json:"name"`
+	Cluster      string `json:"cluster"`
+	StorageClass string `json:"storage_class"`
+	VolumeMode   string `json:"volume_mode"`
+	Storage      string `json:"storage"`
+	AccessMode   string `json:"access_mode"`
+	Path         string `json:"path"`
 }
 
 func (p *pv) CreatePvc(client *kubernetes.Clientset, data *PvCreate) (err error) {
 	amsTemp := strings.Split(data.AccessMode, "/")
 	ams := []corev1.PersistentVolumeAccessMode{}
-	var vmTemp corev1.PersistentVolumeMode
-	vmTemp = corev1.PersistentVolumeMode(data.VolumeMode)
-	for idx, val := range amsTemp {
-		ams[idx] = corev1.PersistentVolumeAccessMode(val)
+	//ams := make([]corev1.PersistentVolumeAccessMode, 10)  这两者长度都默认是0
+
+	//var vmTemp corev1.PersistentVolumeMode
+	vmTemp := corev1.PersistentVolumeMode(data.VolumeMode)
+	for _, val := range amsTemp {
+		//ams[idx] = corev1.PersistentVolumeAccessMode(val)  //得有长度
+		ams = append(ams, corev1.PersistentVolumeAccessMode(val))
 	}
 	pv := &corev1.PersistentVolume{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      data.Name,
-			Namespace: data.Namespace,
+			Name: data.Name,
 		},
 		Spec: corev1.PersistentVolumeSpec{
 			AccessModes: ams,
